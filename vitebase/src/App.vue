6eedaf046d0a2013ce509432043cba2e571eef27
baizhi958216 <1475289190@qq.com>
2022-07-11 21:35:37 +0800

    vue: Composition API initialize
    Signed-off-by: baizhi958216 <1475289190@qq.com>

--- vitebase/src/App.vue
+++ vitebase/src/App.vue
@@ -1,95 +1,11 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <p>{{ text1 }}</p>
-  <button @click="updateval">更新</button>
-  <p>{{ text2 }}</p>
-  <button @click="updateval1">更新</button>
-  <p>
-    {{
-        `今天的天气是：${weather}现在的时间是：${new Date().toISOString()}`
-    }}
-  </p>
-  <p>{{ computedinfo }}</p>
+  <div>
 
-  <!-- components -->
-  <button @click="showDialog = !showDialog">对话框</button>
-  <myDialog 
-  v-model="weather" 
-  :weadatas="weather" 
-  @close="closeDialog"
-  v-if="showDialog">
-    <template v-slot:toat>
-      提示框<br />
-    </template>
-    <template v-slot:weather="childWeather">
-      今天的天气是
-      <span style="color:orange">
-        {{ childWeather.wea }}
-      </span>
-    </template>
-  </myDialog>
+  </div>
 </template>
 
-<script>
-import myDialog from "./components/myDialog.vue";
-export default {
-  components: { myDialog },
-  data() {
-    return {
-      text1: "Vue2",
-      text2: {
-        name: "Vue2",
-        author: "EvanYou",
-      },
-      weather: "晴天",
-      showDialog: false,
-      conf: "父组件调用",
-      title: "当前天气"
-    };
-  },
-  methods: {
-    updateval() {
-      this.text1 = "Vue3";
-    },
-    updateval1() {
-      this.text2.name = "Vue3";
-    },
-    closeDialog(val) {
-      console.log(val);
-      this.showDialog = false;
-    }
-  },
-  watch: {
-    text1(newVlaue, oldValue) {
-      console.log(newVlaue, oldValue);
-    },
-    //当侦听的某个变量值是对象时不起作用
-    //则需要深度监听
-    text2: {
-      handler(newVlaue, oldValue) {
-        console.log(newVlaue, oldValue);
-      },
-      deep: true,
-    },
-    //因为数据同源, text2中newValue跟oldValue值一致
-    //如果想要得到不同的值需要结合计算属性computed。
-    datadiff: {
-      handler(newVlaue, oldValue) {
-        console.log(newVlaue, oldValue);
-      },
-      deep: true,
-    },
-  },
-  computed: {
-    datadiff() {
-      return JSON.parse(JSON.stringify(this.text2));
-    },
-    computedinfo() {
-      return `今天的天气是：${this.weather
-        }现在的时间是：${new Date().toISOString()}`;
-    },
-  },
-};
+<script setup>
+
 </script>
 
 <style>
