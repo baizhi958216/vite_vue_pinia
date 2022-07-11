167763c29183a1524a6a719881cb38945ca8d749
baizhi958216 <1475289190@qq.com>
2022-07-12 04:16:33 +0800

    vue: Import pinia * Pinia! not VueX * 状态管理工具
    Signed-off-by: baizhi958216 <1475289190@qq.com>

--- vitebase/src/main.js
+++ vitebase/src/main.js
@@ -4,6 +4,9 @@ import App from "./App.vue";
 // 引入路由
 import { createRouter, createWebHistory } from "vue-router";
 
+// 引入pinia
+import { createPinia } from 'pinia'
+
 let routes = [
   { path: "/login", component: () => import("./views/Login.vue") },
   { path: "/test", component: () => import("./views/test.vue") },
@@ -29,4 +32,7 @@ const router = createRouter({
   routes,
 });
 
-createApp(App).use(router).mount("#app");
+let app = createApp(App)
+app.use(router)
+app.use(createPinia())
+app.mount("#app")
