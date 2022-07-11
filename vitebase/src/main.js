c7d7c02b50f6110744f789165ae616cf39eab422
baizhi958216 <1475289190@qq.com>
2022-07-12 03:32:03 +0800

    vue: children router
    Signed-off-by: baizhi958216 <1475289190@qq.com>

--- vitebase/src/main.js
+++ vitebase/src/main.js
@@ -5,14 +5,28 @@ import App from "./App.vue";
 import { createRouter, createWebHistory } from "vue-router";
 
 let routes = [
-    {path:'/login',component:()=>import('./views/Login.vue')},
-    {path:'/test',component:()=>import('./views/test.vue')}
-]
+  { path: "/login", component: () => import("./views/Login.vue") },
+  { path: "/test", component: () => import("./views/test.vue") },
+  {
+    path: "/main",
+    component: () => import("./views/Main.vue"),
+    children: [
+      {
+        path: "/main/page1",
+        component: () => import("./views/Page1.vue"),
+      },
+      {
+        path: "/main/page2",
+        component: () => import("./views/Page2.vue"),
+      },
+    ],
+  },
+];
 
 const router = createRouter({
-    // createWebHistory: HTML5模式
-    history: createWebHistory(),
-    routes
-})
+  // createWebHistory: HTML5模式
+  history: createWebHistory(),
+  routes,
+});
 
-createApp(App).use(router).mount("#app")
+createApp(App).use(router).mount("#app");
---
+++ vitebase/src/views/Main.vue
@@ -0,0 +1,28 @@
+<template>
+    <div>
+        <button @click="gotoPage1('page1')">Page1</button>
+        <button @click="gotoPage2('page2')">Page2</button>
+    </div>
+    <div>
+        <router-view></router-view>
+    </div>
+</template>
+<script setup>
+import { useRoute, useRouter } from 'vue-router'
+const route = useRoute()
+const router = useRouter()
+function gotoPage1(page) {
+    router.push(
+        {
+            path: '/main/' + page
+        }
+    )
+}
+function gotoPage2(page) {
+    router.push(
+        {
+            path: '/main/' + page
+        }
+    )
+}
+</script>
