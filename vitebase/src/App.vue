47a222bf05982d32880c03c14921d27d3f2fe29b
baizhi958216 <1475289190@qq.com>
2022-07-12 01:41:20 +0800

    vue: Base composition API
    Signed-off-by: baizhi958216 <1475289190@qq.com>

--- vitebase/src/App.vue
+++ vitebase/src/App.vue
@@ -1,11 +1,37 @@
 <template>
   <div>
-
+    <p>{{ a }}</p>
+    <button @click="changeName">Change</button>
+    <hr>
+    <div v-for="(item, index) of OEM">
+      {{ item.id }}-{{ item.name }}
+    </div>
+    <button @click="changeOEM">OEMChange</button>
   </div>
 </template>
 
 <script setup>
+import { ref, reactive } from 'vue'
+let a = ref('张三')
 
+function changeName() {
+  a.value = '李四'
+}
+
+const OEM = reactive([{
+  id: 1,
+  name: 'ASUS'
+}, {
+  id: 2,
+  name: 'Lenoeo'
+}, {
+  id: 3,
+  name: 'Xiaomi'
+}])
+
+function changeOEM() {
+  OEM[1].name = 'Acer'
+}
 </script>
 
 <style>
